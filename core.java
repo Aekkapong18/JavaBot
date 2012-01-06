2673b31a7461ab7c81b857af882c4ceeb630688c
Tom Powell <zifnab@zifnab06.net>
2012-01-05 19:04:47 -0700

    Changed to Java7, now using switch strings

--- core.java
+++ core.java
@@ -45,22 +45,37 @@ class core {
 			current = in.readLine();
 				if (current == null) System.exit(0);
 				System.out.println(current);
-				if (current.split(" ")[0].equals("PING")){
-					System.out.println("PONG sent");
-					out.println("PONG " + current.split(" ")[1]);
-				} else if (current.split(" ")[1].equals("PRIVMSG")) {
-					if(current.toLowerCase().split(":siri").length == 2){
-						if (current.toLowerCase().contains("google")){
-							String searchTerms = "";
-							searchTerms = current.toLowerCase().split("google")[1];
-							if(!searchTerms.equals(""))
-								out.println("PRIVMSG #" + current.toLowerCase().split("#")[1].split(" ")[0] + " :" + google.search(searchTerms)[1]);
-						} else if (current.toLowerCase().contains("join")){
-							out.println("JOIN " + current.toLowerCase().split("join ")[1]);
-						} else if (current.toLowerCase().contains("source")){
-							out.println("PRIVMSG " + current.toLowerCase().split("#")[1].split(" ")[0] + " :http://github.com/zifnab06/JavaBot");
+				switch(current.split(" ")[0]){
+					case "PING":
+						System.out.println("PONG sent");
+						out.println("PONG " + current.split(" ")[1]);
+						break;
+					default:
+						switch(current.split(" ")[1]){
+							//$nick PRIVMSG $channel :$msg
+							//$nick PRIVMSG $channel :Siri, command excess
+							case "PRIVMSG":
+								if(current.toLowerCase().split(" ")[3].contains("siri")){
+									switch(current.toLowerCase().split(" ")[4]){
+									case "google":
+										String searchTerms = "";
+										searchTerms = current.toLowerCase().split("google ")[1];
+										if(!searchTerms.equals(""))
+											out.println("PRIVMSG #" + current.toLowerCase().split("#")[1].split(" ")[0] + " :" + google.search(searchTerms)[1]);
+										break;
+									case "join":
+										out.println("JOIN " + current.toLowerCase().split("join ")[1]);
+										break;
+									case "source":
+										out.println("PRIVMSG #" + current.toLowerCase().split("#")[1].split(" ")[0] + " :http://github.com/zifnab06/JavaBot");
+										break;
+									}
+								}
+								break;
+							default:
+								break;
 						}
-					}
+					break;
 				}
 			} catch (Exception e){e.printStackTrace();}
 		}
